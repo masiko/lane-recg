f3801ca1f9be66fa1898b9d214788c52ee697a63
masiko <masiko.taiitirou@gmail.com>
2019-05-13 00:03:32 +0900

    fix for opencv3.2

--- demo/DetectLane.hpp
+++ demo/DetectLane.hpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
+#include <opencv2/imgproc/imgproc.hpp>
 
 int test(cv::Mat in) {
 	int pos;
--- demo/main.cpp
+++ demo/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/opencv.hpp>
+//#include <opencv2/highgui/highgui.hpp>
 #include "DetectLane.hpp"
 
 //#define USECAM (1)
@@ -47,7 +47,7 @@ int main() {
 		cv::imshow("edge",edge);
 		cv::imshow("edge_lane",edge_lane);
 		cv::imshow("lane_area",lane_area);
-		if (cv::waitKey(100)!=-1)	break;
+		if (cv::waitKey(100)!=255)	break;	//opencv2.4 waitkey() returned -1 when don't press key, but 3.2 return 255!!!!
 	}
 
 	return 0;
